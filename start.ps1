71fe88c2c3ae8e3d7c37c634a067075531043efb
andrebossi <andreluizbossi70@gmail.com>
2022-08-22 11:13:39 -0300

    Update launch script

--- start.ps1
+++ start.ps1
@@ -1,11 +1,11 @@
 netsh interface portproxy add v4tov4 listenport=27015 listenaddress=192.168.0.180 connectport=27015 connectaddress=10.0.75.1
 
-docker container run ^
-    -it ^
-    --name cs16-server ^
-    --restart on-failure ^
-    --env-file ./cs16-server-launcher.env ^
-    -v d:\steam:/opt/steamcmd ^
-    -p 27015:27015/udp ^
-    cs ^
+docker container run `
+    -it `
+    --name cs16-server `
+    --restart on-failure `
+    --env-file ./cs16-server-launcher.env `
+    -v d:\steam:/opt/steamcmd `
+    -p 27015:27015/udp `
+    cs `
     cs16-server-launcher start
